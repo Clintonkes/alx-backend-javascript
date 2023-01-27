01a14df0ac7a2f5299f4cf73f502eb934bc1aedb
Clintonkes <clintonemeka05@gmail.com>
2023-01-27 11:46:57 +0000

    Basics

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+g
+rt default function createIteratorObject(report) {
+    return (function* _() {
+      for (const department of Object.values(report.allEmployees)) {
+        for (const employee of department) {
+          yield employee;
+        }
+      }
+    }());
+  }
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,1 @@
+This directory talks about the backend part of javascript
